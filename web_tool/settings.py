d52366fe1fe41856addfccea5d5eb1d7167d1147
admin <admin@gmail.com>
2023-02-26 16:55:12 +0530

    remove static urls updated vercel.json

--- web_tool/settings.py
+++ web_tool/settings.py
@@ -156,8 +156,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Google Drive Storage Settings
--- web_tool/urls.py
+++ web_tool/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('ap/', admin.site.urls),
     path('', include("home.urls")),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += staticfiles_urlpatterns()
